20410209e1ffa115c5bf041e6baa50263cd9a1d7
Sam Wellander <samwellander@gmail.com>
2018-10-24 20:08:58 -0400

    fix babel version and setup react entry

--- client/index.js
+++ client/index.js
@@ -1,1 +1,10 @@
-console.log('hey')
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './components/Main';
+
+const App = () => (
+  <Main />
+)
+
+render(<App />, document.getElementById('app'));
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: ['@babel/polyfill', './client'],
+  entry: ['babel-polyfill', './client'],
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public')
