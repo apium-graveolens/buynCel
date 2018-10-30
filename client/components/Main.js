ea51114750fe9634fb18c4e6ee1b557470d40196
Sam Wellander <samwellander@gmail.com>
2018-10-29 20:42:44 -0400

    add dependencies

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 import ProductList from './ProductList';
+import SignUp from './SignUp';
 
 class Main extends Component {
   render = () => {
@@ -8,8 +9,10 @@ class Main extends Component {
       <div>
         <Link to='/'>Home Link</Link>
         <Link to='/products'>Products Link</Link>
+        <Link to='/sign-up'>Sign Up Link</Link>
         <h1>This will be a header</h1>
         <Route exact path='/products' component={ProductList} />
+        <Route exact path='/sign-up' component={SignUp} />
       </div>
     )
   }
