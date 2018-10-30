b4f08a3b5d2b361677c73ce881eb52dcc8594c8b
Sam Wellander <samwellander@gmail.com>
2018-10-29 20:43:11 -0400

    add signup route and some grid styling

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
 import ProductList from './ProductList';
 import SignUp from './SignUp';
 
@@ -7,12 +8,14 @@ class Main extends Component {
   render = () => {
     return (
       <div>
-        <Link to='/'>Home Link</Link>
-        <Link to='/products'>Products Link</Link>
-        <Link to='/sign-up'>Sign Up Link</Link>
-        <h1>This will be a header</h1>
-        <Route exact path='/products' component={ProductList} />
-        <Route exact path='/sign-up' component={SignUp} />
+        <Grid container justify="center">
+          <Link to='/'>Home Link</Link>
+          <Link to='/products'>Products Link</Link>
+          <Link to='/sign-up'>Sign Up Link</Link>
+          <h1>This will be a header</h1>
+          <Route exact path='/products' component={ProductList} />
+          <Route exact path='/sign-up' component={SignUp} />
+        </Grid>
       </div>
     )
   }
