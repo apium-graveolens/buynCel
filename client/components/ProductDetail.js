b9cf6ad5b739ca091dc109cc27dfa955214130db
Sam Wellander <samwellander@gmail.com>
2018-10-31 12:05:50 -0400

    add product detail view

---
+++ client/components/ProductDetail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { connect } from 'react-redux';
+
+const ProductDetail = ({ product }) => {
+  const { title, description, price, photo } = product;
+  return (
+    <Grid container justify="center">
+      <Grid item xs={12} lg={3}>
+        <img src={photo} />
+      </Grid>
+      <Grid item xs={12} lg={3}>
+        <h3>{title}</h3>
+        <hr />
+        <p>{description}</p>
+        <p>${price}</p>
+      </Grid>
+    </Grid>
+  )
+};
+
+const mapStateToProps = ({ products }, { match }) => {
+  console.log('mapping in detail')
+  const id = match.params.id * 1;
+  return {
+    product: products.find(product => product.id == id)
+  }
+}
+
+export default connect(mapStateToProps)(ProductDetail);
