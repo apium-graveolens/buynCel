1d601a0872233fcddbdf877c5db2f7c77840103c
Sam Wellander <samwellander@gmail.com>
2018-11-03 09:59:05 -0400

    update nav

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { Menu, MenuItem, Button, Grid, withStyles, IconButton, Typography, AppBar, Toolbar } from '@material-ui/core';
+import { ListItemIcon, Divider, List, ListItem, ListItemText, Drawer, Menu, MenuItem, Button, Grid, withStyles, IconButton, Typography, AppBar, Toolbar } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import ListAlt from '@material-ui/icons/ListAlt';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
 import MenuIcon from '@material-ui/icons/Menu';
 import { connect } from 'react-redux';
 import { logout } from '../store/auth';
@@ -36,27 +38,58 @@ const styles = {
 
 class NavBar extends Component {
   state = {
-    anchorEl: null,
-    anchorAccount: null,
-  }
-  handleClick = e => {
-    this.setState({ anchorEl: e.currentTarget })
+    // anchorAccount: null,
+    left: false,
+    right: false,
   };
-  handleAccountMenu = e => {
-    this.setState({ anchorAccount: e.currentTarget })
-  }
-  handleAccountClose = () => {
-    this.setState({ anchorAccount: null });
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
   };
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
+
+  // handleClick = e => {
+  //   this.setState({ anchorEl: e.currentTarget })
+  // };
+  // handleAccountMenu = e => {
+  //   this.setState({ anchorAccount: e.currentTarget })
+  // }
+  // handleAccountClose = () => {
+  //   this.setState({ anchorAccount: null });
+  // };
+  // handleClose = () => {
+  //   this.setState({ anchorEl: null });
+  // };
   render() {
     const { classes, auth } = this.props;
     const { anchorEl, anchorAccount } = this.state;
     const open = Boolean(anchorEl);
     const accountOpen = Boolean(anchorAccount);
     const isHome = this.props.location.pathname == '/';
+
+
+    const leftMenu = (
+      <List>
+        <ListItem button component={Link} to="/products">
+          <ListItemIcon><ListAlt /></ListItemIcon>
+          <ListItemText primary='Products' />
+        </ListItem>
+        <ListItem button component={Link} to="/cart">
+          <ListItemIcon><ShoppingCart /></ListItemIcon>
+          <ListItemText primary='Cart' />
+        </ListItem>
+        <Divider />
+        {auth.id ? (
+          <ListItem button>
+            <ListItemIcon><AccountCircle /></ListItemIcon>
+            <ListItemText primary='Account' />
+          </ListItem>
+        ) : (
+            ''
+          )}
+      </List>
+    )
     return (
       <div className={classes.root}>
         <AppBar className={classes.appBar} position="static" color="default">
@@ -70,13 +103,13 @@ class NavBar extends Component {
                 <IconButton
                   className={classes.menuButton}
                   color="inherit" aria-label="Menu"
-                  onClick={this.handleClick}
+                  onClick={this.toggleDrawer('left', true)}
                 >
                   <MenuIcon
                     fontSize="large"
                   />
                 </IconButton>
-                <Menu
+                {/* <Menu
                   id="fade-menu"
                   anchorEl={anchorEl}
                   open={open}
@@ -84,7 +117,17 @@ class NavBar extends Component {
                 >
                   <MenuItem component={Link} to="/products" onClick={this.handleClose}>Products</MenuItem>
                   <MenuItem component={Link} to="/sign-up" onClick={this.handleClose}>Sign Up</MenuItem>
-                </Menu>
+                </Menu> */}
+                <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
+                  <div
+                    tabIndex={0}
+                    role="button"
+                    onClick={this.toggleDrawer('left', false)}
+                    onKeyDown={this.toggleDrawer('left', false)}
+                  >
+                    {leftMenu}
+                  </div>
+                </Drawer>
                 {!isHome && (
                   <Typography className={classes.title} variant="h6" color="inherit" to='/' component={Link}>
                     Celery
@@ -104,7 +147,7 @@ class NavBar extends Component {
                       Login
                   </Button>
                   )}
-                <Menu
+                {/* <Menu
                   id="account-menu"
                   anchorEl={anchorAccount}
                   open={accountOpen}
@@ -116,7 +159,7 @@ class NavBar extends Component {
                   </MenuItem>
 
                   <MenuItem onClick={this.props.logout}>Logout</MenuItem>
-                </Menu>
+                </Menu> */}
               </Grid>
             </Grid>
           </Toolbar>
