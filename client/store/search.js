419de366e0a73311b052a91e9a5afc5259215b03
Sam Wellander <samwellander@gmail.com>
2019-01-04 12:16:47 -0800

    add suggestions to search reducer

--- client/store/search.js
+++ client/store/search.js
@@ -17,11 +17,11 @@ const setSuggestions = suggestions => {
 export const _searchTerm = term => dispatch => {
   axios.get(`/api/search/${term}`)
     .then(res => res.data)
-    .then(searchResults => console.log(searchResults))
+    .then(searchResults => dispatch(setSuggestions(searchResults)))
     .catch(err => console.log('something went wrong with _searchTerm thunk', err))
 }
 
-export default (state = [], action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case SETSUGGESTIONS:
       const { suggestions } = action;
