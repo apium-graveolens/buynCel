05d72e1fe22bcb87d277679e443b5735831a127f
Sam Wellander <samwellander@gmail.com>
2019-01-17 16:40:12 -0500

    create new cart order after an order has been placed

--- client/components/ShippingInfoForm.js
+++ client/components/ShippingInfoForm.js
@@ -4,30 +4,6 @@ import { Typography, FormGroup, TextField, Grid, Checkbox } from '@material-ui/c
 import { _editOrder } from '../store/orders';
 
 class ShippingInforForm extends Component {
-  // state = {
-  //   name: null,
-  //   address: null,
-  //   addressCity: null,
-  //   addressState: null,
-  //   addressZip: null,
-  //   save: false
-  // }
-
-  // componentDidMount() {
-  //   //if currently logged in user has saved shipping information, set state to that information
-  //   const { auth } = this.props;
-  //   if (auth.user.id) {
-  //     const { name, addressCity, addressState, addressZip, address } = auth.user;
-  //     this.setState({
-  //       name,
-  //       addressCity,
-  //       addressState,
-  //       addressZip,
-  //       address
-  //     })
-  //   }
-  // }
-
   render() {
     const { shippingInfo, handleShippingInfoChange } = this.props;
     const { name, address, addressCity, addressState, addressZip, save } = shippingInfo;
