f9b6b0b92734340108dfe13740280fe57b0b0e9d
Sam Wellander <samwellander@gmail.com>
2019-01-17 11:00:41 -0500

    more styling

--- client/components/ProductDetail.js
+++ client/components/ProductDetail.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReviewList from './ReviewList';
-import { withStyles, Chip, Grid, Button, Typography } from '@material-ui/core';
+import { withStyles, Chip, Grid, Button, Typography, TextField } from '@material-ui/core';
 import { connect } from 'react-redux';
 import CategoryChip from './CategoryChip';
+import { _createLineItem, _updateLineItem, _removeLineItem } from '../store/orders';
 
 const styles = {
   img: {
@@ -11,62 +12,86 @@ const styles = {
   reviewContainer: {
     height: '30vh'
   },
+  numInput: {
+    width: 35
+  }
 };
 
-const ProductDetail = ({ product, classes }) => {
-  const { title, description, price, photo, categories } = product;
-  return (
-    //TODO: add spacing/padding to grid items
-    <Grid spacing={40} container justify="center">
-      <Grid item xs={10} sm={5} md={4} lg={3}>
-        <img
-          className={classes.img}
-          src={photo}
-        />
-      </Grid>
-      <Grid item xs={10} sm={5} md={4} lg={3}>
-        <Typography variant="title">{title}</Typography>
-        <hr />
-        <Typography>{description}</Typography>
-        <Typography>${price}</Typography>
+class ProductDetail extends React.Component {
+  state = {
+    quantity: 0
+  }
+  handleClick = () => {
+    const { history, create, auth, product, orders } = this.props;
+    const order = orders.find(order => order.status === 'cart');
+    create(order.id, product.id, auth.user.id)
+      .then(thing => {
+        console.log(thing);
+        history.push('/cart');
+      });
 
-        {categories && categories.map(category => (
-          <CategoryChip key={category.id} category={category} />
-        ))}
+    // history.push('/cart')
+  }
+  changeQuantity = e => {
+    this.setState({ quantity: e.target.value })
+  }
+  render() {
+    const { product, classes, auth } = this.props;
+    const { title, description, price, photo, categories } = product;
+    return (
+      //TODO: add spacing/padding to grid items
+      <Grid spacing={40} container justify="center">
+        <Grid item xs={10} sm={5} md={4} lg={3}>
+          <img
+            className={classes.img}
+            src={photo}
+          />
+        </Grid>
+        <Grid item xs={10} sm={5} md={4} lg={3}>
+          <Typography variant="title">{title}</Typography>
+          <hr />
+          <Typography>{description}</Typography>
+          <Typography>${price}</Typography>
 
-        <hr />
-        <h4>Add to cart</h4>
+          {categories && categories.map(category => (
+            <CategoryChip key={category.id} category={category} />
+          ))}
 
-        <div>
-          <Button value="plus" size="small" color="primary">
-            +
-          </Button>
-          <Typography>
-            0
-          </Typography>
-          <Button
-            value="minus" size="small"
-            color="primary"
-          >
-            -
-          </Button>
-        </div>
+          <hr />
 
-      </Grid>
-      <Grid justify="center" container className={classes.reviewContainer} item xs={12}>
-        <Grid item xs={11} lg={8}>
-          <ReviewList product={product} />
+          {auth.user.id && (
+            <div>
+              <Button
+                onClick={this.handleClick}
+                variant="contained"
+                color="primary"
+              >Add To Cart</Button>
+            </div>
+          )}
+
+        </Grid>
+        <Grid justify="center" container className={classes.reviewContainer} item xs={12}>
+          <Grid item xs={11} lg={8}>
+            <ReviewList product={product} />
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  )
+    )
+  }
 };
 
-const mapStateToProps = ({ products }, { match }) => {
+const mapStateToProps = ({ products, auth, orders }, { match }) => {
   const id = match.params.id * 1;
   return {
-    product: products[0] ? products.find(product => product.id == id) : {}
+    product: products[0] ? products.find(product => product.id == id) : {},
+    orders,
+    auth,
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    create: (orderId, productId, userId) => dispatch(_createLineItem(orderId, productId, userId))
   }
 }
 
-export default withStyles(styles)(connect(mapStateToProps)(ProductDetail));
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(ProductDetail));
